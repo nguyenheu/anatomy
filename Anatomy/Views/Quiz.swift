d3247700fe1f435deb3152ece1314600d101b9ec
Nguyễn Quốc Hiếu <nguyenquochieu0604@gmail.com>
2024-03-11 23:26:05 +0700

    commit message

--- Anatomy/Views/Quiz.swift
+++ Anatomy/Views/Quiz.swift
@@ -40,7 +40,7 @@ struct Quiz: View {
                     Button {
                         anatomyManager.goToNextQuestion()
                     } label: {
-                        Text("Next abc")
+                        Text("Next abcxyz")
                             .font(.fontRoboto(.bold, fontSize: 18))
                             .foregroundColor(Color.white)
                             .padding()
